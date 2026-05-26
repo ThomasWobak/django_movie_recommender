9c6569a5c625aa80269a5cbbc69622837a6bcac6
Thomas <t1wobak@edu.aau.at>
2026-05-26 14:57:13 +0200

    initial preprocessing and data ingestion

--- recommender/models.py
+++ recommender/models.py
@@ -7,7 +7,11 @@ class Movie(models.Model):
     plot = models.TextField(blank=True)
     poster_url = models.URLField(max_length=500, blank=True)
     popularity = models.IntegerField(default=0)
+    average_rating = models.FloatField(default=0.0)
     genres = models.CharField(max_length=255, blank=True)
+    actors = models.CharField(max_length=500, blank=True)
+    directors = models.CharField(max_length=255, blank=True)
+    reviews = models.TextField(blank=True)
 
 class SimilarityResult(models.Model):
     source_movie = models.ForeignKey(Movie, related_name='similarities', on_delete=models.CASCADE)
